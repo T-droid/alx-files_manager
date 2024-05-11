5406c5635eaddb0c014bc6d4c9199fdb7be98e9f
MurungaOwen <owenhood80@gmail.com>
2024-05-11 13:04:41 +0300

    setup redis client

--- utils/redis.js
+++ utils/redis.js
@@ -1,2 +1,57 @@
-import { promisify } from 'util';
-import { createClient } from 'redis';
+import redis from 'redis';
+import { promisify } from 'util'
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+
+        this.client.on('error', (err) => {
+            console.error('Redis connection error', err);
+        });
+    }
+
+    isAlive() {
+        // returns true if client is connected
+        return this.client.connected;
+    }
+
+    async get(key) {
+        // get value of the key
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    async set(key, value, durationInSeconds) {
+        // set value for a key and also time for expiry
+        return new Promise((resolve, reject) => {
+            this.client.set(key, value, 'EX', durationInSeconds, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
