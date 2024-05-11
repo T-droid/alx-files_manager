c553ea4f3a1852a66ed909fd057345f77337a4d2
MurungaOwen <owenhood80@gmail.com>
2024-05-11 18:23:44 +0300

    setup server

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import express from 'express';
 import routes from './routes/index.js';
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.set('port', port);
 
-app.use('', routes);
+app.use('/', routes);
 
 app.listen(app.get('port'), () => {
     console.log(`Server running on port: ${port}`);
