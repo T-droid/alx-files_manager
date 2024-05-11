a570bd7eaaf96a5508eb2d9c38df57b6288191ef
emmanuel tindi <emmanueltindi66@gmail.com>
2024-05-11 14:22:56 +0300

    created mongo client

---
+++ utils/db.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require('mongodb');
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || '27017';
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${HOST}:${PORT}`;
+class DBClient {
+    constructor() {
+        // Set host, port, and database based on environment variables
+        this.client = new MongoClient(url, { useUnifiedTopology: true });
+        this.client.connect();
+    }
+
+    //checks if the mongo client is connected succesfully
+    isAlive() {
+        return this.client.isConnected()
+    
+    }
+
+    //counts the number of documents of the collection users
+    async nbUsers() {
+        try {
+            const db = this.client.db(this.database);
+            const users = db.collection('users');
+            return await users.countDocuments();
+        } catch (err) {
+            console.error('Error counting documents in "users" collection');
+        }
+    }
+
+    //count the number of documents in the collection files
+    async nbFiles() {
+        try {
+            const db = this.client.db(this.database);
+            const files = db.collection('files');
+            return await files.countDocuments();
+        } catch (err) {
+            console.error('Error counting documents in "files" collection');
+        }
+    }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
