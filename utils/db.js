10e50883cecedd36745432d6cb4992b62254f8a0
emmanuel tindi <emmanueltindi66@gmail.com>
2024-05-11 14:23:28 +0300

    created mongo client

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,13 @@
 const { MongoClient } = require('mongodb');
 
+//get environmental variables
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || '27017';
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
+
 const url = `mongodb://${HOST}:${PORT}`;
+
+
 class DBClient {
     constructor() {
         // Set host, port, and database based on environment variables
